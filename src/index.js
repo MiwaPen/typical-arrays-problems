e6cd2b4d29782bddf2bfba12902ab7c80e7454f2
unknown <gig405@mail.ru>
2020-10-03 20:37:47 +0300

    feat: update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 
 exports.min = function min (array) {
+if (array== undefined || array.length==0) {
   return 0;
+}
+  let minEl=array[0];
+for (let index = 0; index < array.length; index++) {
+  if (minEl>array[index]) {
+    minEl=array[index]
+  }
+}
+  return minEl;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array== undefined || array.length==0) {
+    return 0;
+  }
+  let maxEl=array[0];
+  for (let index = 0; index < array.length; index++) {
+    if (maxEl<array[index]) {
+      maxEl=array[index]
+    }
+  }
+    return maxEl;
+
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array== undefined || array.length==0) {
+    return 0;
+  }
+  let sum= 0;
+  for (let index = 0; index < array.length; index++) {
+    sum = sum+array[index]
+  }
+  return sum/array.length;
 }
